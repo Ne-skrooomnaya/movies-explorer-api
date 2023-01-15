71c4c67f68a208098ded23bdabfceda4b255360a
ne_skrooomnaya <angelf04032005@gmail.com>
2023-01-15 19:17:25 +0300

    diplom/server

--- middlewares/ErrorHandler.js
+++ middlewares/ErrorHandler.js
@@ -1,6 +1,6 @@
 const ErrorHandler = ((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = statusCode === 500 ? 'Ошибка на сервере' : err.message;
+  const message = statusCode === 500 ? 'Ошибка на сервере666' : err.message;
   res.status(statusCode).send({ message });
   next();
 });
