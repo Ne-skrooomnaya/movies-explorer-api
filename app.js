cb8908274afb76e5d0389b39ed02c00b2751ef3e
ne_skrooomnaya <angelf04032005@gmail.com>
2023-01-15 04:02:03 +0300

    diplom/local

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const cors = require('cors');
-// const { cors } = require('./middlewares/cors');
+const { cors } = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 const ErrorHandler = require('./middlewares/ErrorHandler');
@@ -13,15 +12,9 @@ const apiLimiter = require('./middlewares/apiLimiter');
 const { PORT = 3002, DB_ADRESS = 'mongodb://localhost:27017/moviesdb' } = process.env;
 const app = express();
 
-app.use(
-  cors({
-    origin: ['http://localhost:3000', 'http://localhost:3002', 'https://angelDiplomnaya.nomoredomains.club', 'https://api.angelDiplomnaya.nomoredomains.club'], // было 3000
-    credentials: true,
-  }),
-);
-
 app.use(requestLogger);
 app.use(apiLimiter);
+app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
