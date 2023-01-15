1964f56a07f101478145576b723169ecfb43d457
ne_skrooomnaya <angelf04032005@gmail.com>
2023-01-15 19:14:10 +0300

    diplom/server

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -13,7 +13,7 @@ module.exports.cors = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', 'no-cors');
   }
 
   if (method === 'OPTIONS') {
